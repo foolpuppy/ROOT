c2be35134f1b964dc30f2af2ad5fbb80738e6a5a
L <1610940520@qq.com>
2019-04-29 20:56:31 +0800

    JSON 操作

--- src/top/wigon/servlet/mainPage/init.java
+++ src/top/wigon/servlet/mainPage/init.java
@@ -2,6 +2,7 @@ package top.wigon.servlet.mainPage;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 import top.wigon.entity.Item;
 import top.wigon.service.impl.ItemServiceImpl;
@@ -42,19 +43,40 @@ public class init extends HttpServlet {
     @Test
     void test() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
         ItemServiceImpl itemService = new ItemServiceImpl();
         //推荐产品
         List<Item> items_0f = itemService.findItemsByCategoryCols("电子产品", 5);
-        List<Item> items_1f = itemService.findItemsByCategory("衣橱服务");
-        List<Item> items_2f = itemService.findItemsByCategory("生鲜水果");
-        List<Item> items_3f = itemService.findItemsByCategory("美妆馆");
-        objectMapper.writeValueAsString(items_0f);
+        List<Item> items_1f = itemService.findItemsByCategoryCols("衣橱服务", 5);
+        List<Item> items_2f = itemService.findItemsByCategoryCols("生鲜水果", 5);
+        List<Item> items_3f = itemService.findItemsByCategoryCols("美妆馆", 5);
+        List<Item> items_all = itemService.findItemsByCategoryCols("美妆馆", 20);
+        String JSON = null;
         StringBuffer content = new StringBuffer();
         content.append("{\"status\": 0,\"floor\": {\"bigimg\": \"res/static/img/s_img1.jpg\",");
         content.append("\"banner\": {");
         content.append("\"cont\":");
-        content.append(items_0f);
-        System.err.println(content);
+        JSON = objectMapper.writeValueAsString(items_0f);
+        content.append(JSON);
+        content.append("}},");
+        content.append("\"proct\": [{\"floorNum\": \"1F\",\"productItem\": \"product-item1\",\"carImg\": \"res/static/img/icon_gou.png\", \"text\": \"电子产品\",\"bigImg\": \"res/static/img/img12.jpg\",\n" +
+                " \"imgBox\":");
+        JSON = objectMapper.writeValueAsString(items_1f);
+        content.append(JSON);
+        content.append("},");
+        content.append("{\"floorNum\": \"2F\",\"productItem\": \"product-item2\",\"carImg\": \"res/static/img/icon_gou.png\",\"text\": \"服饰衣帽\",\"bigImg\": \"res/static/img/img12.jpg\",\"imgBox\": ");
+        JSON = objectMapper.writeValueAsString(items_2f);
+        content.append(JSON);
+        content.append("},");
+        content.append("{\"floorNum\": \"3F\",\"productItem\": \"product-item3\",\"carImg\": \"res/static/img/icon_gou.png\", \"text\": \"化妆用品\",\"bigImg\": \"res/static/img/img12.jpg\",\"imgBox\": ");
+        JSON = objectMapper.writeValueAsString(items_3f);
+        content.append(JSON);
+        content.append("}],");
+        content.append("\"productList\": ");
+        JSON = objectMapper.writeValueAsString(items_all);
+        content.append(JSON);
+        content.append("}");
+        System.err.println(content.toString());
 
     }
 }
