d3e79ec95e02af9ae6797e5395e9fca581b32b94
L <1610940520@qq.com>
2019-04-29 19:58:14 +0800

    商品图片更新SERVLET 商品描述更新Servlet

--- src/top/wigon/servlet/mainPage/init.java
+++ src/top/wigon/servlet/mainPage/init.java
@@ -1,10 +1,13 @@
 package top.wigon.servlet.mainPage;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import top.wigon.entity.Item;
 import top.wigon.service.impl.ItemServiceImpl;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,16 +19,42 @@ import java.util.List;
  * @version 1.0
  * @date 2019/4/29 19:18
  **/
-public class init  extends HttpServlet {
+@WebServlet("/init")
+public class init extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper objectMapper=new ObjectMapper();
-        ItemServiceImpl itemService=new ItemServiceImpl();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ItemServiceImpl itemService = new ItemServiceImpl();
         //推荐产品
-        List<Item> items_0f=itemService.findItemsByCategoryCols("电子产品",5);
-        List<Item> items_1f=itemService.findItemsByCategory("衣橱服务");
-        List<Item> items_2f= itemService.findItemsByCategory("生鲜水果");
-        List<Item> items_3f=itemService.findItemsByCategory("美妆馆");
+        List<Item> items_0f = itemService.findItemsByCategoryCols("电子产品", 5);
+        List<Item> items_1f = itemService.findItemsByCategory("衣橱服务");
+        List<Item> items_2f = itemService.findItemsByCategory("生鲜水果");
+        List<Item> items_3f = itemService.findItemsByCategory("美妆馆");
+        objectMapper.writeValueAsString(items_0f);
+        StringBuffer content = new StringBuffer();
+        content.append("{\"status\": 0,\"floor\": {\"bigimg\": \"res/static/img/s_img1.jpg\",");
+        content.append("\"banner\": {");
+        content.append("\"cont\":");
+        content.append(items_0f);
+        System.out.println();
+    }
+
+    @Test
+    void test() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ItemServiceImpl itemService = new ItemServiceImpl();
+        //推荐产品
+        List<Item> items_0f = itemService.findItemsByCategoryCols("电子产品", 5);
+        List<Item> items_1f = itemService.findItemsByCategory("衣橱服务");
+        List<Item> items_2f = itemService.findItemsByCategory("生鲜水果");
+        List<Item> items_3f = itemService.findItemsByCategory("美妆馆");
+        objectMapper.writeValueAsString(items_0f);
+        StringBuffer content = new StringBuffer();
+        content.append("{\"status\": 0,\"floor\": {\"bigimg\": \"res/static/img/s_img1.jpg\",");
+        content.append("\"banner\": {");
+        content.append("\"cont\":");
+        content.append(items_0f);
+        System.err.println(content);
 
     }
 }
